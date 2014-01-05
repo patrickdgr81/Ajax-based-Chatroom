d993628494703566fd49c7bc3eabff44b688f9f1
Patrick Lu <patricklu00@Hotmail.com>
2014-01-04 20:42:25 -0800

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_HOST','mysql17.000webhost.com');
 define('DB_USER','a2684778_Patrick');
-define('DB_PASSWORD','g0ldc4ndy');
+define('DB_PASSWORD','');
 define('DB_DATABASE','a2684778_chatD');
 
 ?>
